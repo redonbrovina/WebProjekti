d02829863874ed10e9ffc8199e1818f411872b93
Redon <redonbrovina@gmail.com>
2024-12-07 23:57:26 +0100

    Registration Validation

--- register-form.js
+++ register-form.js
@@ -1,14 +1,38 @@
-let usernameEl = document.getElementById("username");
-let passwordEl = document.getElementById("password");
-let emailEl = document.getElementById("email");
-let gender = document.getElementsByName("Gender");
-let errorEl = document.getElementById("error-txt");
+const usernameEl = document.getElementById("username");
+const passwordEl = document.getElementById("password");
+const emailEl = document.getElementById("email");
+let checked_gender = document.getElementsByName("gender");
+const phone = document.getElementById("phone");
+const errorEl = document.getElementById("error-txt");
 const url = "./index.html";
 
+const emailValid = (email) => {
+    const emailCheck = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/;
+    return emailCheck.test(email.toLowerCase());
+}
+
+function isChecked() {
+    for(let radio of checked_gender){
+        if(radio.checked){
+            return true;
+        }
+    }
+    return false;
+}
+
 function enterData() {
-    if(usernameEl.value != "" && passwordEl.value != "" && emailEl.value != "" && (gender[0].checked == true || gender[1].checked == true)){
-        location.href = url;
+    if(usernameEl.value == ""){
+        errorEl.textContent = "Please write a valid username";
+        usernameEl.focus();
+    }else if(passwordEl.value == ""){
+        errorEl.textContent = "Please write a valid password";
+        passwordEl.focus();
+    }else if(!emailValid(emailEl.value)){
+        errorEl.textContent = "Please write a valid Email!";
+        emailEl.focus();
+    }else if(!isChecked()){
+        errorEl.textContent = "Please check a gender";
     }else{
-        errorEl.textContent = "Inputs may be empty or invalid!";
+        location.href = url;
     }
 }
